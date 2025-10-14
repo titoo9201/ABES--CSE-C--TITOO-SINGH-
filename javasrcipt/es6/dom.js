cf87edbb343455f3986b432f5dd6fd1c238efc55
titoo9201 <titoos675@gmail.com>
2025-10-14 15:17:15 +0530

    dom

---
+++ javasrcipt/es6/dom.js
@@ -0,0 +1,6 @@
+let div= document.createElement("div")
+let h1 = document.createElement("h1")
+h1.innerText="python is create ai ";
+div.appendChild (h1);
+let id=document.querySelector("#baap");
+id.appendChild(div);
