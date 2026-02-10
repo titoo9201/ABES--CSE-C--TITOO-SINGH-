16a0b3f959f6e637615d6c1bcc2b7575ae60c481
titoo9201 <titoos675@gmail.com>
2026-02-10 12:52:48 +0530

    fs module structure

--- 10feb/server.js
+++ 10feb/server.js
@@ -1,43 +1,43 @@
-const http = require("http")
-const fs= require("fs")
+// const http = require("http")
+// const fs= require("fs")
 
-const server= http.createServer((req,res)=>{
-    if(req.url=="/")
-    {
-    fs.readFile("index.html","utf8",(err,htmlContent)=>{
-        if(err)
-        {
-            res.statusCode=500;
-            res.end("internal server error")
-            return 
-        }
-        res.setHeader("content-Type","text/html");
-        res.end(htmlContent)
-    })
-    }
-    else if (req.url=="/style.css")
-    {
-        fs.readFile("style.css","utf8",(err,cssContent)=>{
-            if(err)
-            {
-                res.statusCode=500;
-                res.end("internal server error ")
-                return 
-            }
-              res.setHeader("content-Type","text/css");
-              res.end(cssContent)
-        })
+// const server= http.createServer((req,res)=>{
+//     if(req.url=="/")
+//     {
+//     fs.readFile("index.html","utf8",(err,htmlContent)=>{
+//         if(err)
+//         {
+//             res.statusCode=500;
+//             res.end("internal server error")
+//             return 
+//         }
+//         res.setHeader("content-Type","text/html");
+//         res.end(htmlContent)
+//     })
+//     }
+//     else if (req.url=="/style.css")
+//     {
+//         fs.readFile("style.css","utf8",(err,cssContent)=>{
+//             if(err)
+//             {
+//                 res.statusCode=500;
+//                 res.end("internal server error ")
+//                 return 
+//             }
+//               res.setHeader("content-Type","text/css");
+//               res.end(cssContent)
+//         })
 
-    }
+//     }
 
-    else{
-        res.statusCode=404
-       res.end("not found")
-    }
-})
+//     else{
+//         res.statusCode=404
+//        res.end("not found")
+//     }
+// })
 
-const port =3000
-server.listen(port,()=>{
-    console.log("server is running on the port ",port);
+// const port =3000
+// server.listen(port,()=>{
+//     console.log("server is running on the port ",port);
     
-})
+// })
